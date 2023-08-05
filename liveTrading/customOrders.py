9f5d273f5b60308af449dd96758c93c7d451d7e6
Nikita Olechko <nikita.olechko@gmail.com>
2023-08-05 00:45:42 -0700

    Separate customOrders from liveTradingUtilities

--- liveTrading/customOrders.py
+++ liveTrading/customOrders.py
@@ -36,6 +36,41 @@ def bracketSellOrder(quantity=1, profitTarget=1.02, stopLoss=0.99, orderId=1):
     return bracketOrders
 
 
+def bracketBuyOrder(quantity=1, profitTarget=1.02, stopLoss=0.99, orderId=1):
+    # Create Parent Order / Initial Entry
+    parent = Order()
+    parent.orderId = orderId
+    parent.orderType = "MTK"
+    parent.action = "SELL"
+    parent.totalQuantity = quantity
+    parent.transmit = False
+    parent.eTradeOnly = False
+    parent.firmQuoteOnly = False
+    # Profit Target Order
+    profitTargetOrder = Order()
+    profitTargetOrder.orderId = orderId + 1
+    profitTargetOrder.orderType = "LMT"
+    profitTargetOrder.action = "BUY"
+    profitTargetOrder.totalQuantity = quantity
+    profitTargetOrder.lmtPrice = round(profitTarget, 2)
+    profitTargetOrder.transmit = True
+    profitTargetOrder.eTradeOnly = False
+    profitTargetOrder.firmQuoteOnly = False
+    # Stop Loss Order
+    stopLossOrder = Order()
+    stopLossOrder.orderId = orderId + 2
+    stopLossOrder.orderType = "STP"
+    stopLossOrder.action = "BUY"
+    stopLossOrder.totalQuantity = quantity
+    stopLossOrder.auxPrice = round(stopLoss, 2)
+    stopLossOrder.transmit = True
+    stopLossOrder.eTradeOnly = False
+    stopLossOrder.firmQuoteOnly = False
+    # Bracket Orders Array
+    bracketOrders = [parent, profitTargetOrder, stopLossOrder]
+    return bracketOrders
+
+
 def marketBuyOrder(orderId, quantity=1):
     # Create order object
     order = Order()
--- liveTrading/liveTradingUtilities.py
+++ liveTrading/liveTradingUtilities.py
@@ -23,12 +23,31 @@ def holding_gross_return(barDataFrame, current_average):
 def check_current_position(ib, ticker):
     positions = ib.positions()
     for position in positions:
-        if ticker is not None:
-            if position.contract.symbol == ticker:
-                return {ticker: {"Position": position.position, "Average Cost": position.avgCost,
-                                 "Market Value": position.marketPrice}}
+        if position.contract.symbol == ticker:
+            return {ticker: {"Position": position.position, "Average Cost": position.avgCost,
+                             "Market Value": position.marketPrice}}
     return {}
 
 
-def calculate_current_return(barDataFrame, current_average, last_order_index):
-    pass
+def get_order_history(ib, ticker):
+    order_history = ib.fills()
+    order_history_dict = {}
+    for order in order_history:
+        if order.contract.symbol == ticker:
+            order_name = ticker + "-" + str(order.execution.orderId) + "-" + order.execution.time.strftime(
+                "%Y-%m-%d-%H:%M:%S")
+            order_history_dict[order_name] = {
+                "Ticker": order.contract.symbol,
+                "Order Id": order.execution.orderId,
+                "Order Action": order.execution.side,
+                "Order Quantity": order.execution.shares,
+                "Order Price": order.execution.price,
+                "Order Average Price": order.execution.avgPrice,
+                "Order Fill Price": order.execution.price,
+                "Order Fill Time": order.execution.time.strftime("%Y-%m-%d-%H:%M:%S"),
+                "Order Commission": order.commissionReport.commission}
+    return order_history_dict
+
+
+def get_time_of_day_as_int(time):
+    return int(time.strftime("%H%M%S"))
