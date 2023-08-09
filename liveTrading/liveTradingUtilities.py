19fead3cad7806dc9652f428f5e0feda0998e798
Nikita Olechko <nikita.olechko@gmail.com>
2023-08-09 15:51:50 -0700

    Revert "Separate customOrders from liveTradingUtilities"
    This reverts commit 9f5d273f5b60308af449dd96758c93c7d451d7e6.

--- liveTrading/liveTradingUtilities.py
+++ liveTrading/liveTradingUtilities.py
@@ -23,31 +23,12 @@ def holding_gross_return(barDataFrame, current_average):
 def check_current_position(ib, ticker):
     positions = ib.positions()
     for position in positions:
-        if position.contract.symbol == ticker:
-            return {ticker: {"Position": position.position, "Average Cost": position.avgCost,
-                             "Market Value": position.marketPrice}}
+        if ticker is not None:
+            if position.contract.symbol == ticker:
+                return {ticker: {"Position": position.position, "Average Cost": position.avgCost,
+                                 "Market Value": position.marketPrice}}
     return {}
 
 
-def get_order_history(ib, ticker):
-    order_history = ib.fills()
-    order_history_dict = {}
-    for order in order_history:
-        if order.contract.symbol == ticker:
-            order_name = ticker + "-" + str(order.execution.orderId) + "-" + order.execution.time.strftime(
-                "%Y-%m-%d-%H:%M:%S")
-            order_history_dict[order_name] = {
-                "Ticker": order.contract.symbol,
-                "Order Id": order.execution.orderId,
-                "Order Action": order.execution.side,
-                "Order Quantity": order.execution.shares,
-                "Order Price": order.execution.price,
-                "Order Average Price": order.execution.avgPrice,
-                "Order Fill Price": order.execution.price,
-                "Order Fill Time": order.execution.time.strftime("%Y-%m-%d-%H:%M:%S"),
-                "Order Commission": order.commissionReport.commission}
-    return order_history_dict
-
-
-def get_time_of_day_as_int(time):
-    return int(time.strftime("%H%M%S"))
+def calculate_current_return(barDataFrame, current_average, last_order_index):
+    pass
