075fbe0bf54209fd203f1ea0287e23f46e649996
Nikita Olechko <nikita.olechko@gmail.com>
2023-08-09 18:55:23 -0700

    rename main backtesting file

--- backtesting/backtestingSample.py
+++ backtesting/backtestingSample.py
@@ -2,10 +2,11 @@ from ib_insync import IB
 
 from backtestingUtilities.simulationUtilities import run_strategy_on_list_of_tickers
 from strategies.greaterthan60barsma import sampleSMABuySellStrategy, generate60PeriodSMA_backtest
+from utilities.generalUtilities import get_tws_connection_id
 
 ib = IB()
 try:
-    ib.connect('127.0.0.1', 4000, clientId=50)
+    ib.connect('127.0.0.1', 4000, clientId=get_tws_connection_id())
 except Exception:
     print("Could not connect to IBKR. Check that Trader Workstation or IB Gateway is running.")
 
