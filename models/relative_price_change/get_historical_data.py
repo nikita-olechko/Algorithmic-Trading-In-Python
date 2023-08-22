01ba539e3b436c931162a312c566a2407b0fe6bb
Nikita Olechko <nikita.olechko@gmail.com>
2023-08-22 15:40:45 -0700

    Create get_historical_data.py

---
+++ models/relative_price_change/get_historical_data.py
@@ -0,0 +1,7 @@
+from utilities.generalUtilities import get_months_of_historical_data, initialize_ib_connection
+
+ib = initialize_ib_connection()
+# data = get_months_of_historical_data(ib, 'XOM', months=12, directory_offset=2)
+
+for ticker in ["NVDA", "AAPL", "TSLA", "AMZN", "GOOG", "FB", "MSFT", "NFLX", "INTC", "AMD"]:
+    get_months_of_historical_data(ib, ticker, months=12, directory_offset=2)
