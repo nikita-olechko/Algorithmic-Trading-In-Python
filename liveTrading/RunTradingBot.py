7d969e2afc27fe145745cc26067fff534bf207cf
Nikita Olechko <nikita.olechko@gmail.com>
2023-08-29 10:39:50 -0700

    Separate running bot instance from class location

--- liveTrading/RunTradingBot.py
+++ liveTrading/RunTradingBot.py
@@ -3,4 +3,5 @@ from strategies.greaterthan60barsma import sampleSMABuySellStrategy, generate60P
 
 # Start Bot(s)
 bot1 = Bot(symbol="AAPL", quantity=1, buySellConditionFunc=sampleSMABuySellStrategy,
-           generateNewDataFunc=generate60PeriodSMAWholeDataFrame)
+           generateNewDataFunc=generate60PeriodSMAWholeDataFrame,
+           last_row_only=False, periods_to_analyze=65, operate_on_minute_data=True)
