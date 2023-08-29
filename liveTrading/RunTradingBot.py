781def1b37f51f739430e546ee8288204312c22f
Nikita Olechko <nikita.olechko@gmail.com>
2023-08-29 11:29:28 -0700

    Update backtesting sample to 10 periods from 60

--- liveTrading/RunTradingBot.py
+++ liveTrading/RunTradingBot.py
@@ -1,7 +1,7 @@
 from liveTrading.LiveTradingBot import Bot
-from strategies.greaterthan60barsma import sampleSMABuySellStrategy, generate60PeriodSMAWholeDataFrame
+from strategies.greaterthan10barsma import sampleSMABuySellStrategy, generate10PeriodSMAWholeDataFrame
 
 # Start Bot(s)
-bot1 = Bot(symbol="AAPL", quantity=1, buySellConditionFunc=sampleSMABuySellStrategy,
-           generateNewDataFunc=generate60PeriodSMAWholeDataFrame,
-           last_row_only=False, periods_to_analyze=65, operate_on_minute_data=True)
+bot = Bot(symbol="AAPL", quantity=1, buySellConditionFunc=sampleSMABuySellStrategy,
+          generateNewDataFunc=generate10PeriodSMAWholeDataFrame,
+          last_row_only=False, periods_to_analyze=65, operate_on_minute_data=True)
