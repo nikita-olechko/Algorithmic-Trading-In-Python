568d35c4424ae3edfe30d9925707cf1c5b65431b
Nikita Olechko <nikita.olechko@gmail.com>
2023-08-30 13:45:01 -0700

    Update backtestingSample

--- backtesting/backtestingSample.py
+++ backtesting/backtestingSample.py
@@ -4,10 +4,12 @@ from strategies.greaterthan10barsma import sampleSMABuySellStrategy, generate10P
 strategy_name = '10PeriodSMA_Test'
 strategy_buy_or_sell_condition_function = sampleSMABuySellStrategy
 generate_additional_data_function = generate10PeriodSMAWholeDataFrame
+barsize = "1 min"
+duration = "1 M"
 
 run_strategy_on_list_of_tickers(strategy_name,
                                 strategy_buy_or_sell_condition_function=strategy_buy_or_sell_condition_function,
                                 generate_additional_data_function=generate_additional_data_function,
-                                barsize="1 min", duration="1 M",
+                                barsize=barsize, duration=duration,
                                 very_large_data=False, try_errored_tickers=True
                                 )
