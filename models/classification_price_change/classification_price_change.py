fe6710b032e0109d230e942f98fc51a4dbb72cf8
nikita-olechko <nikita.olechko@gmail.com>
2023-09-03 17:34:34 -0700

    Add allowable error for detection

--- models/classification_price_change/classification_price_change.py
+++ models/classification_price_change/classification_price_change.py
@@ -111,9 +111,10 @@ def boolean_bollinger_band_location(minuteDataFrame):
 
 
 def price_change_over_next_Z_periods_greater_than_X_boolean(dataFrame, periods, percentage_change):
-    dataFrame['Max_Price_in_Next_Z_Periods'] = dataFrame['Average'].rolling(periods).max().shift(-(periods-1))
+    dataFrame['Max_Price_in_Next_Z_Periods'] = dataFrame['Average'].rolling(periods).max().shift(-(periods - 1))
     dataFrame[f'maximum_percentage_price_change_over_next_{periods}_periods_greater_than_{percentage_change}'] = \
-        ((dataFrame['Max_Price_in_Next_Z_Periods'] - dataFrame['Average']) / dataFrame['Average']) * 100 >= percentage_change
+        ((dataFrame['Max_Price_in_Next_Z_Periods'] - dataFrame['Average']) / dataFrame[
+            'Average']) * 100 >= percentage_change
     dataFrame.drop(['Max_Price_in_Next_Z_Periods'], axis=1, inplace=True)
     return dataFrame
 
@@ -279,7 +280,7 @@ def create_classification_price_change_mlp_model(symbol, endDateTime='', save_mo
 
 def analyze_classification_model_performance(ticker, model_object, test_data, additional_columns_to_remove=None,
                                              Z_periods=60,
-                                             X_percentage=3, model_type='lm'):
+                                             X_percentage=3, model_type='lm', allowable_error=0):
     """
     Analyze the performance of a predictive model.
 
@@ -321,6 +322,24 @@ def analyze_classification_model_performance(ticker, model_object, test_data, ad
     results['Residual'] = results['Actual'].astype(int) - results['Predicted'].astype(int)
     results['Incorrect_Detections'] = (results["Residual"] == -1).astype(int)
 
+    data['Max_Price_in_Next_Z_Periods'] = data['Average'].rolling(Z_periods).max().shift(-(Z_periods - 1))
+    data[f'maximum_percentage_price_change_over_next_{Z_periods}'] = \
+        ((data['Max_Price_in_Next_Z_Periods'] - data['Average']) / data[
+            'Average']) * 100
+
+    strictly_incorrect_detection_indices = incorrect_detections_not_within_Z_periods_of_correct_detection(results,
+                                                                                                          Z_periods)
+
+    detections_within_error = []
+    detections_outside_error = []
+
+    for index in strictly_incorrect_detection_indices:
+        max_percentage_price_change = data[f'maximum_percentage_price_change_over_next_{Z_periods}'][index]
+        if max_percentage_price_change >= (X_percentage * allowable_error / 100):
+            detections_within_error.append(index)
+        else:
+            detections_outside_error.append(index)
+
     def correctly_predicted_change(actual, predicted):
         if actual == 1:
             return predicted
@@ -355,17 +374,24 @@ def analyze_classification_model_performance(ticker, model_object, test_data, ad
                        "Number_Of_Total_Occurences": results['Actual'].sum(),
                        "Number_Of_Correct_Detections": results['Correctly_Predicted_Change'].sum(),
                        "Number_Of_Incorrect_Detections": (results["Residual"] == -1).sum(),
-                       f"Number_of_Occurences_More_Than_{Z_periods}_Periods_Apart": len(
+                       f"Number_of_Grouped_Occurences": len(
                            occurences_more_than_Z_periods_apart(
                                results, column_name='Actual', Z_periods=Z_periods)),
-                       f"Number_of_Detections_More_Than_{Z_periods}_Periods_Apart": len(
+                       f"Number_of_Grouped_Detections": len(
                            occurences_more_than_Z_periods_apart(
                                results, column_name='Predicted', Z_periods=Z_periods)),
-                       f"Number_of_Correct_Detections_More_Than_{Z_periods}_Periods_Apart":
+                       f"Number_of_Grouped_Correct_Detections":
                            len(occurences_more_than_Z_periods_apart(results, column_name='Correctly_Predicted_Change'
                                                                     , Z_periods=Z_periods)),
-                       f"Number_of_Incorrect_Detections_Not_Within_{Z_periods}_Periods_Of_Correct_Detection":
+                       f"Number_of_Grouped_Correct_Detections_Within_Error": len(detections_within_error) +
+                                                                             len(occurences_more_than_Z_periods_apart(
+                                                                                 results,
+                                                                                 column_name='Correctly_Predicted_Change'
+                                                                                 , Z_periods=Z_periods)),
+                       f"Number_of_Grouped_Strictly_Incorrect_Detections":
                            len(incorrect_detections_not_within_Z_periods_of_correct_detection(results, Z_periods)),
+                       f"Number_of_Grouped_Incorrect_Detections_Outside_Error":
+                           len(detections_outside_error),
                        "Above_2SD_Correctly_Predicted": above_two_sd_series.sum(),
                        "Above_1SD_Correctly_Predicted": above_one_sd_series.sum(),
                        "Below_2SD_Correctly_Predicted": below_two_sd_series.sum(),
--- models/classification_price_change/classification_utilities.py
+++ models/classification_price_change/classification_utilities.py
@@ -5,11 +5,15 @@ from itertools import groupby, count
 
 def occurences_more_than_Z_periods_apart(dataframe, column_name='Actual', Z_periods=60):
     indices = list(dataframe[dataframe[column_name] == 1].index)
+    if len(indices) == 0:
+        return []
     separate_occurences = []
+    first_occurence_in_sequence = indices[0]
     for index, occurence in enumerate(indices):
         if index + 1 < len(indices):
             if indices[index + 1] - occurence > Z_periods:
-                separate_occurences.append(occurence)
+                separate_occurences.append(first_occurence_in_sequence)
+                first_occurence_in_sequence = indices[index + 1]
         else:
             separate_occurences.append(occurence)
     return separate_occurences
@@ -25,6 +29,15 @@ def incorrect_detections_not_within_Z_periods_of_correct_detection(dataframe, Z_
         if abs(closest_value - detection) > Z_periods:
             incorrect_detections.append(detection)
     return incorrect_detections
+
+
+def incorrect_detection_within_allowable_error(results_dataframe, max_percent_change, Z_periods, X_percentage, allowable_error):
+
+    # we need the predicted values
+    # we need the actual percentages
+    predicted_indices = list(results_dataframe[results_dataframe['Predicted'] == 1].index)
+    percent_changes_at_predicted_indices = results_dataframe.loc[predicted_indices, 'Percent Change']
+
 
 
 def create_classification_report_name(Z_periods=60, X_percentage=3, model_type='lm'):
