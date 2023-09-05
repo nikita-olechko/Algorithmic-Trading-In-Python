007fa4b5dfb88997f83e5490aa69c7f1a1cdad47
Nikita Olechko <nikita.olechko@gmail.com>
2023-09-04 22:18:37 -0700

    Modularize model strategy utilities

---
+++ utilities/model_strategy_utilities.py
@@ -0,0 +1,18 @@
+def predict_based_on_model(barDataFrame, model_object):
+    x_columns = list(barDataFrame.columns)
+
+    always_redundant_columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Average', 'Barcount', 'Orders',
+                                'Position']
+
+    for column in always_redundant_columns:
+        x_columns.remove(column)
+
+    data = barDataFrame.dropna()
+
+    x_test = data[x_columns]
+
+    predict_price_change = model_object.predict(x_test)
+    predict_price_change = predict_price_change.reshape(-1, 1)
+    predict_price_change = pd.DataFrame(predict_price_change, columns=['Prediction'])
+    barDataFrame['Prediction'] = predict_price_change
+    return barDataFrame
