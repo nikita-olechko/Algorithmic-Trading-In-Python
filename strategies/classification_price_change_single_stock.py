ee3cbf9d7e1452b72187a1ce071627129bbc62ca
Nikita Olechko <nikita.olechko@gmail.com>
2023-09-07 22:20:17 -0700

    Update classification price strategy with new data

--- strategies/classification_price_change_single_stock.py
+++ strategies/classification_price_change_single_stock.py
@@ -1,13 +1,15 @@
 from utilities.dataGenerationUtilities import create_price_variables, create_log_price_variables, \
-    create_volume_change_variables, generate_bollinger_bands, boolean_bollinger_band_location
-from utilities.general_strategy_utilities import profit_taker, minutes_since_last_order
+    generate_bollinger_bands, boolean_bollinger_band_location, days_crossover, \
+    create_volume_variables
+from utilities.general_strategy_utilities import profit_taker, minutes_since_last_order, stop_loss
 from utilities.model_strategy_utilities import predict_based_on_model
 
 
 def generate_model_data(barDataFrame, model_object=None, Z_periods=120, periodicity=1):
+    barDataFrame = days_crossover(barDataFrame, Z_periods)
     barDataFrame = create_log_price_variables(barDataFrame, list_of_periods=range(1, Z_periods, periodicity))
     barDataFrame = create_price_variables(barDataFrame, list_of_periods=range(1, Z_periods, periodicity))
-    barDataFrame = create_volume_change_variables(barDataFrame, list_of_periods=range(1, Z_periods, periodicity))
+    barDataFrame = create_volume_variables(barDataFrame, list_of_periods=range(1, Z_periods, periodicity))
     barDataFrame = generate_bollinger_bands(barDataFrame)
     barDataFrame = boolean_bollinger_band_location(barDataFrame)
     barDataFrame = predict_based_on_model(barDataFrame, model_object)
@@ -20,6 +22,8 @@ def classification_model_strategy(barDataFrame, last_order_index=0, current_inde
     """
     current_prediction = barDataFrame.loc[barDataFrame.index[current_index], 'Prediction']
     if barDataFrame["Orders"][last_order_index] == 1:
+        if stop_loss(barDataFrame, last_order_index, current_index, 2.0) & current_prediction != 1:
+            return -1
         if profit_taker(barDataFrame, last_order_index, current_index, 1.5):
             if current_prediction == 1:
                 return 3
